01800a3cacde2a9307146ff12a12c9f6aa8b578b
platypus333 <qtinghao@163.com>
2021-05-26 18:20:56 +0800

    1~2.3

--- 数据结构(交大)/数据结构(交大)/DataStructure.swift
+++ 数据结构(交大)/数据结构(交大)/DataStructure.swift
@@ -1,10 +1,3 @@
-//
-//  DataStructure.swift
-//  数据结构(交大)
-//
-//  Created by EJU on 2021/5/25.
-//
-
 import Foundation
 
 class DataStructure {
@@ -21,6 +14,29 @@ class DataStructure {
 //
 extension DataStructure
 {
+    //百钱买鸡 公鸡5钱、母鸡3钱、鸡雏1钱3
+    //穷举法
+    func buyChicken()
+    {
+        var n = 0, z : Int
+        for x in 0...20 {
+            for y in 0...33 {
+                z = (100 - x - y)
+                if (x*5+y*3+z/3 == 100) && (z%3 == 0)
+                {
+                    print("公鸡 = \(x), 母鸡 = \(y), 鸡雏 = \(z)")
+                    n+=1
+                }
+            }
+        }
+        print("共\(n)种方法")
+    }
+    
+    func aaa()
+    {
 
+    }
+    
+    
     
 }
